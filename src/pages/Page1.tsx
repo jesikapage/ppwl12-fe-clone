085dbf01ab027d8748ad1d4ef5c2678a109053dd
fcnwy <vionadaniellabirathn@gmail.com>
2026-04-29 21:24:47 +0700

    footer main page

--- src/pages/Page1.tsx
+++ src/pages/Page1.tsx
@@ -1,7 +1,10 @@
+// pages/Page1.tsx
+import FooterLinks from '../components/footer';
 export default function Page1() {
     return (
         <div className="h-[calc(100vh-4rem)] flex justify-center items-center">
             <h1>Page 1</h1>
+            <FooterLinks/>
         </div>
     )
 }
