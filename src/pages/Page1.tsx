4ff8d05bb834f2419c633efae0a9215d8338c8eb
fcnwy <vionadaniellabirathn@gmail.com>
2026-04-29 22:06:31 +0700

    Memperbaiki footer main page

--- src/pages/Page1.tsx
+++ src/pages/Page1.tsx
@@ -4,10 +4,9 @@ export default function Page1() {
     return (
         <div className="flex flex-col min-h-screen">
             <div className="flex-grow flex justify-center items-center py-20">
-                <h1>Konten Utama Page 1 (Main Page)</h1>
+                <h1 className="text-2xl font-bold text-[#222222]">Konten Utama Page 1 (Main Page)</h1>
             </div>
-
-            <InsipirasiLiburan/>
+            {/* Jika komponen InspirasiLiburan sudah Anda buat, letakkan di sini */}
         </div>
     )
 }
